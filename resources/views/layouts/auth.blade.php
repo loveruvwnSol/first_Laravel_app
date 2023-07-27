8869e8363ca5861606fa5f1a9a4fd755fce2acf8
sol <ocosio0907@gmail.com>
2023-07-27 14:38:51 +0900

    :recycle: Refactoring

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,10 +8,6 @@
     <title>Document</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
 </head>
 
 <body>
